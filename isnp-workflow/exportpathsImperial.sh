a27d3a0c56f604e24d2fac6bad666a45da8737ac
Balazs Bohar <bbohar@ic-5cg3423gzn>
2024-09-24 10:42:36 +0100

    Create an Imperial HPC specific export paths file

---
+++ isnp-workflow/exportpathsImperial.sh
@@ -0,0 +1,6 @@
+export PATH=$PATH:/rds/general/user/bbohar/home/bedtools2/bin
+export PATH=$PATH:/rds/general/user/bbohar/home/vcftools-0.1.16/src/cpp
+export PATH=$PATH:/rds/general/user/bbohar/home/meme-5.1.1/src
+export PATH=$PATH:/rds/general/user/bbohar/home/meme-5.1.1/scripts
+export PATH=$PATH:/rds/general/user/bbohar/home/
+export PERL5LIB=/rds/general/user/bbohar/home/meme-5.1.1/scripts:$PERL5LIB
